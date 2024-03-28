03282d82e59dd98100a162951831142cc9c356d0
Juha Lindstedt <juha@pakastin.fi>
2024-03-29 01:47:38 +0200

    Dockerize

--- server/index.js
+++ server/index.js
@@ -7,13 +7,14 @@ const PORT = NODE_PORT || 8080;
 const app = express();
 
 app.set('view engine', 'pug');
-app.set('views', 'client');
+app.set('views', '/app/client');
+app.disable('x-powered-by');
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
-app.use(express.static('public'));
+app.use(express.static('/app/public'));
 
 app.listen(PORT, (err) => {
   if (err) {
